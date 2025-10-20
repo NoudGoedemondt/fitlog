c2eee95ee8a047431e92a1a1fcd91df3cd99b988
Noud Goedemondt <noudgoedemondt@gmail.com>
2025-10-20 19:33:43 +0200

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <v-app>
+    <v-main>
+      <v-btn></v-btn>
+      <header>
+        <div class="wrapper">
+          <HelloWorld msg="You did it!" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+          <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+          </nav>
+        </div>
+      </header>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import '@mdi/font/css/materialdesignicons.css';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
+  },
+});
 
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(vuetify).use(router).mount('#app');
