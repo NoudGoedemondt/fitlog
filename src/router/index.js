9171377c1ea444603d490d2dfc80c7d25e7c0cad
Noud <noudgoedemondt@gmail.com>
2026-09-01 16:28:50 +0200

    added nav guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/auth.js'
+import { watch } from 'vue'
+
+function waitForAuthReady(authStore) {
+  return new Promise((resolve) => {
+    if (!authStore.isLoading) {
+      resolve()
+      return
+    }
+    const unwatch = watch(
+      () => authStore.isLoading,
+      (loading) => {
+        if (!loading) {
+          unwatch()
+          resolve()
+        }
+      },
+    )
+  })
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,31 +29,50 @@ const router = createRouter({
       component: () => import('../views/HomeView.vue'),
     },
     {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+    },
+    {
       path: '/body-profile',
       name: 'body-profile',
       component: () => import('../views/BodyProfileView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/meals',
       name: 'meals',
       component: () => import('../views/MealsView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/routines',
       name: 'routines',
       component: () => import('../views/RoutinesView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/weight-logs',
       name: 'weight-logs',
       component: () => import('../views/WeightLogsView.vue'),
+      meta: { requiresAuth: true },
     },
     {
       path: '/workouts',
       name: 'workouts',
       component: () => import('../views/WorkoutsView.vue'),
+      meta: { requiresAuth: true },
     },
   ],
+})
+
+router.beforeEach(async (to, _from) => {
+  const authStore = useAuthStore()
+  await waitForAuthReady(authStore)
+
+  if (to.meta?.requiresAuth && !authStore.user) {
+    return { name: 'login' }
+  }
 })
 
 export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <p>This is the login view.</p>
+  </div>
+</template>
