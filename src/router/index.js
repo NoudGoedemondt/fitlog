f74fbf4cbca3e838e00bceaeef6223966b9e0d1c
Noud <noudgoedemondt@gmail.com>
2026-09-01 16:34:23 +0200

    added register view

--- src/router/index.js
+++ src/router/index.js
@@ -34,6 +34,11 @@ const router = createRouter({
       component: () => import('../views/LoginView.vue'),
     },
     {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue'),
+    },
+    {
       path: '/body-profile',
       name: 'body-profile',
       component: () => import('../views/BodyProfileView.vue'),
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <p>This is the register view.</p>
+  </div>
+</template>
