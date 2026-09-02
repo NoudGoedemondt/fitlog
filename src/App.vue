f4a35c3cda7b73e9e2678f7a8c06b02cb4688fa8
Noud <noudgoedemondt@gmail.com>
2026-09-02 16:34:20 +0200

    added reactive nav bar items, based on user log in

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import { onMounted } from 'vue'
+import { onMounted, computed } from 'vue'
 import { supabase } from './plugins/supabase.js'
 import { useAuthStore } from './stores/auth.js'
 
@@ -12,6 +12,14 @@ const handleLogout = async () => {
     console.error('Error logging out:', error)
   }
 }
+
+const showSignedOutButtons = computed(() => {
+  return !authStore.isLoading && !authStore.user
+})
+
+const showSignedInButtons = computed(() => {
+  return !authStore.isLoading && authStore.user
+})
 
 onMounted(async () => {
   await authStore.init()
@@ -28,20 +36,28 @@ onMounted(async () => {
       <v-spacer></v-spacer>
 
       <v-btn variant="text" to="/">Home</v-btn>
-      <v-btn variant="text" to="/workouts">Workouts</v-btn>
-      <v-btn variant="text" to="/routines">Routines</v-btn>
-      <v-btn variant="text" to="/meals">Meals</v-btn>
-      <v-btn variant="text" to="/weight-logs">Weight Logs</v-btn>
-      <v-btn variant="text" to="/body-profile">Body Profile</v-btn>
+      <v-btn v-if="showSignedInButtons" variant="text" to="/workouts">Workouts</v-btn>
+      <v-btn v-if="showSignedInButtons" variant="text" to="/routines">Routines</v-btn>
+      <v-btn v-if="showSignedInButtons" variant="text" to="/meals">Meals</v-btn>
+      <v-btn v-if="showSignedInButtons" variant="text" to="/weight-logs">Weight Logs</v-btn>
+      <v-btn v-if="showSignedInButtons" variant="text" to="/body-profile">Body Profile</v-btn>
 
       <v-menu>
         <template v-slot:activator="{ props }">
-          <v-btn v-bind="props">
-            {{ authStore.user?.user_metadata?.username }}
+          <v-btn v-bind="props" prepend-icon="mdi-account-circle">
+            {{ authStore.user?.user_metadata?.username || 'Account' }}
           </v-btn>
         </template>
         <v-list>
-          <v-list-item @click="handleLogout">Log Out</v-list-item>
+          <v-list-item v-if="showSignedOutButtons" to="/register" prepend-icon="mdi-account-plus"
+            >Register</v-list-item
+          >
+          <v-list-item v-if="showSignedOutButtons" to="/login" prepend-icon="mdi-account"
+            >Log In</v-list-item
+          >
+          <v-list-item v-if="showSignedInButtons" @click="handleLogout" prepend-icon="mdi-logout"
+            >Log Out</v-list-item
+          >
         </v-list>
       </v-menu>
 
