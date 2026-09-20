41aa34d12dfa1d67f286dd3186240bb45c33441f
Noud <noudgoedemondt@gmail.com>
2026-09-20 20:21:32 +0200

    added weight logs view

--- src/views/WeightLogsView.vue
+++ src/views/WeightLogsView.vue
@@ -4,8 +4,26 @@ import { useWeightLogs } from '../composables/useWeightLogs.js'
 
 const weightLogs = useWeightLogs()
 
+const headers = [
+  { title: 'Id', key: 'id' },
+  { title: 'User Id', key: 'user_id' },
+  { title: 'Weight (kg)', key: 'weight_kg' },
+  { title: 'Measured at', key: 'measured_at' },
+  { title: 'Created at', key: 'created_at' },
+  { title: 'Updated at', key: 'updated_at' },
+  { title: 'Actions', key: 'actions', align: 'end', sortable: false },
+]
+
+function createNewRecord() {
+  return {
+    weight_kg: '',
+    measured_at: '',
+  }
+}
+
 const dialogTarget = ref(false) // false | 'new' | <id> — single source of truth
 const dialogIsOpen = ref(false) // controls dialog *visibility*
+const formModel = ref(createNewRecord())
 
 const openDialog = (target) => {
   dialogTarget.value = target
@@ -28,30 +46,72 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="text-center pa-4">
-    <v-btn @click="openDialog('new')" prepend-icon="mdi-plus"> Add </v-btn>
-    <v-btn @click="openDialog(7)" prepend-icon="mdi-pencil-outline"> Update </v-btn>
+  <v-data-table
+    :headers="headers"
+    :items="weightLogs.weightEntries.value"
+    :loading="weightLogs.isFetchLoading.value"
+  >
+    <template v-slot:top>
+      <v-toolbar>
+        <v-toolbar-title>
+          <v-icon color="medium-emphasis" icon="mdi-scale-bathroom" size="x-small" start></v-icon>
 
-    <v-data-table
-      :items="weightLogs.weightEntries.value"
-      :loading="weightLogs.isFetchLoading.value"
-    ></v-data-table>
+          Weight Entries
+        </v-toolbar-title>
 
-    <v-dialog v-model="dialogIsOpen" width="auto" @after-leave="resetTargetAfterLeave">
-      <v-card max-width="400" prepend-icon="mdi-update">
-        <v-card-title>{{ dialogModeLabel }} Weight Entry</v-card-title>
+        <v-btn class="me-2" prepend-icon="mdi-plus" text="Add" @click="openDialog('new')"></v-btn>
+      </v-toolbar>
+    </template>
 
-        <v-card-text
-          >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem,
-          ullam.</v-card-text
-        >
+    <!-- using dynamic slot-syntax because
+    eslint complains about: v-slot:item.actions="{ item }"
+    -->
+    <template #[`item.actions`]="{ item }">
+      <div class="d-flex ga-2 justify-end">
+        <v-icon
+          color="medium-emphasis"
+          icon="mdi-pencil"
+          size="small"
+          @click="openDialog(item.id)"
+        ></v-icon>
 
-        <template v-slot:actions>
-          <v-btn class="ms-auto" @click="dialogIsOpen = false">
-            {{ dialogModeLabel }}
-          </v-btn>
-        </template>
-      </v-card>
-    </v-dialog>
-  </div>
+        <v-icon
+          color="medium-emphasis"
+          icon="mdi-delete"
+          size="small"
+          @click="remove(item.id)"
+        ></v-icon>
+      </div>
+    </template>
+  </v-data-table>
+
+  <v-dialog v-model="dialogIsOpen" max-width="400" @after-leave="resetTargetAfterLeave">
+    <v-card prepend-icon="mdi-update" :title="`${dialogModeLabel} Weight Entry`">
+      <template v-slot:text>
+        <v-date-input
+          v-model="formModel.measured_at"
+          input-format="dd-mm-yyyy"
+          label="Date"
+          class="mt-5"
+        ></v-date-input>
+
+        <v-number-input
+          v-model="formModel.weight_kg"
+          :precision="2"
+          :min="0"
+          label="Weight (kg)"
+        ></v-number-input>
+      </template>
+
+      <v-divider></v-divider>
+
+      <v-card-actions class="bg-surface-light">
+        <v-btn text="Cancel" variant="plain" @click="dialogIsOpen = false"></v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn :text="dialogModeLabel"></v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
